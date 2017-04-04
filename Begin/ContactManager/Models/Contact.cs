2b15a0462930280742b3ea41196ce0bd5cd65412
DenisVajak <dvajak@etfos.hr>
2017-04-04 13:03:02 +0200

    Task3

---
+++ Begin/ContactManager/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {[!code-csharp[Main](build-restful-apis-with-aspnet-web-api/samples/sample2.cs)]
+    }
+}
